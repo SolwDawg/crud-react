8adb4ab288e530f5e7459cee5b39d79c993dc55e
SolwDawg <113430055+SolwDawg@users.noreply.github.com>
2023-07-27 23:07:10 +0700

    paginate

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,48 +1,76 @@
 import Table from 'react-bootstrap/Table';
 import { useEffect, useState } from 'react';
 import { fetchAllUser } from '../services/userService';
+import ReactPaginate from 'react-paginate';
 
 function TableUsers() {
 
     const [listUsers, setListUsers] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);    
 
     useEffect(() => {
-        getUsers();
+        getUsers(1);
     }, [])
 
+    const handlePageClick = (event) => {
+        getUsers(+event.selected + 1);
+    }
 
-    const getUsers = async () => {
-        let res = await fetchAllUser();
-        if (res && res.data && res.data.data) {
-            setListUsers(res.data.data)
+    const getUsers = async (pages) => {
+        let res = await fetchAllUser(pages);
+        if (res && res.data) {
+            setTotalPages(res.total_pages)
+            setListUsers(res.data)
         }
     }
 
     return (
-        <Table responsive striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Email</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {listUsers && listUsers.length > 0 &&
-                    listUsers.map((user, index) => {
-                        return (
-                            <tr key={`users-${index}`}>
-                                <td>{user.id}</td>
-                                <td>{user.email}</td>
-                                <td>{user.first_name}</td>
-                                <td>{user.last_name}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </Table>
+        <>
+            <Table responsive striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((user, index) => {
+                            return (
+                                <tr key={`users-${index}`}>
+                                    <td>{user.id}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.first_name}</td>
+                                    <td>{user.last_name}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+            <ReactPaginate
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
+        </>
     )
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import axios from './customize-axios';
 
-const fetchAllUser = () => {
-    return axios.get("https://reqres.in/api/users?page=1")
+const fetchAllUser = (pages) => {
+    return axios.get(`/api/users?page=${pages}`)
 }
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
 
 export { fetchAllUser };
